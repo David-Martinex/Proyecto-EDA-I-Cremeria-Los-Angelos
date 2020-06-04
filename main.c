e0c72ff966d07155dee5acd2815e5209086e8219
David-Martinex <franciscotvgamer@gmail.com>
2020-06-03 21:12:06 -0500

    Trate de corregir Algunas cosas de la parte de Stocks e implemente una version mejorada de lo que proponia Alan

--- main.c
+++ main.c
@@ -52,7 +52,7 @@ bool DLL_IsEmpty( DLL* this );
 void DLL_MakeEmpty( DLL* this );
 bool DLL_Search( DLL* this, int bar_code );
 void DLL_Traverse( DLL* this, void (*pfun)( void ) );
-DLL_PeekBack( this->list );
+//DLL_PeekBack( DLL* this );
 
 
 //Definiciones de las funciones de DLL
@@ -100,7 +100,6 @@ void DLL_Delete( DLL** this )
             list->first = next;
             --list->len;
         }
-        
         free( list );
         *this = NULL;
     }
@@ -127,7 +126,6 @@ void DLL_InsertBack( DLL* this, Producto* p, size_t cant )
 Producto* DLL_RemoveBack( DLL* this )
 {
     assert( this );
-    
     if( !DLL_IsEmpty( this ) ){
         Producto* p = ( Producto* )malloc( sizeof ( Producto ) );
         if( p ){
@@ -135,18 +133,16 @@ Producto* DLL_RemoveBack( DLL* this )
             p->precio = this->last->item.precio;
             strcpy( p->nombre, this->last->item.nombre );
 
-            NodePtr
-        }
-        
-        NodePtr tmp = this->last->prev;
-        free( this->last );
-        this->last = tmp;
-        
-        if( NULL != this->last ){
-            this->last->next = NULL;
-            --this->len;
-        }else{
-            reset( this );
+            NodePtr tmp = this->last->prev;
+            free( this->last );
+            this->last = tmp;
+
+            if( NULL != this->last ){
+                this->last->next = NULL;
+                --this->len;
+            }else{
+                reset( this );
+            }
         }
     }
     return p;
@@ -198,9 +194,8 @@ void DLL_Traverse( DLL* this, void (*pfun)( void ) )
 {
     /** Pendiente */
     assert( this );
-    
     for( NodePtr it = this->first; it != NULL; it = it->next ){
-        //pfun( it);;
+        //pfun( it );
     }
 }
 
@@ -228,11 +223,10 @@ StockPtr Stock_new()
    // ...
 }
 
-void Stock_delete( Stock* this )
+void Stock_Delete( StockPtr this )
 {
    // si *this existe{
     assert( this );
-    
     if( this ){
         
     }
@@ -241,7 +235,7 @@ void Stock_delete( Stock* this )
 
 }
 
-void Stock_add( Stock* this, Producto* p, size_t cant )
+void Stock_add( StockPtr this, Producto* p, size_t cant )
 {
    // if( DLL_Search( this->list, p->bar_code ) == NULL ){
    //     DLL_InsertBack( this->list, p, cant );
@@ -254,14 +248,14 @@ void Stock_add( Stock* this, Producto* p, size_t cant )
 }
 
 // quita a un elemento de la lista
-void Stock_remove( Stock* this, Producto* p )
+void Stock_remove( StockPtr this, Producto* p )
 {
    // if( DLL_Search( this->list, p->bar_code ) != NULL ){
    //     DLL_RemoveBack( this->list->cursor );
    // } 
 }
 
-bool Stock_search( Stock* this, Producto* p )
+bool Stock_search( StockPtr this, Producto* p )
 {
    return DLL_Search( this->list, p->bar_code );
 }
@@ -273,14 +267,69 @@ bool Stock_search_by_bar_code( Stock* this, int bar_code )
 
 
 // devuelve una copia del producto al que apunta el 'cursor'
-Producto Stock_get( Stock* this )
+Producto Stock_get( StockPtr this )
 {
    return DLL_PeekBack( this->list );
 }
 
-void Stock_report( Stock* this )
+void Stock_report( StockPtr this )
 {
    // imprime todos los productos de la lista
+}
+
+/*@brief Menu Principal del programa, El usuario debera escojer Alguna de las 6 opciones presentadas.
+ *
+ *@param this Referencia a un objeto DLL.
+ *@param data_back El valor (data) leido del nodo. Si la funcion regresa false, 
+ *entonces el valor de data_back no tiene sentido y no deberia ser usado.
+ *
+ *@return true si la lista NO estaba vacia, o el cursor No apunta a NuLL DLL_IsEmpty
+ *el valor se pudo extraer; false si la lista estaba vacia o si el cursor 
+ *apuntaba a Null.*/
+
+void Stock_Menu(StockPtr stock )
+{
+   int opc;
+    do{
+        //El usuario debera de escojer Alguna de las 6 opciones presentadas a continuacion.
+        printf("\n\a\t====={ Cremeria Los Angelos }=====\n ==> Menu");
+        printf("\nBienvenido, en que le podemos ayudar ?:\n1.-Vender Producto\n2.-Surtir el Almacen de un Producto
+        \n3.-Agregar Un Nuevo Producto\n4.-Desventurar un Producto\n5.-Ver el Inventario completo\n6.-Salir
+        \nFavor de escribir el numero de la opcion de su Preferencia:\t");
+        switch ( opc )
+        {
+        case 1:
+            //Vender Producto/s
+            
+            break;
+        case 2:
+            //Aumentar la cantidad de unidades que se tiene
+            
+            break;
+        case 3:
+            //Agregar un nuevo producto
+            Producto* p;
+            p->
+
+            break;
+        case 4:
+            //Quitar un producto ya existente
+            
+            break;
+        case 5:
+            
+            break;
+        default:
+            printf("\nFavor de ingresar un Numero correspondiente a las opciones presentadas del 1 -> 6");
+            break;
+        }
+    }while(opc != 6 )
+}
+
+void Stack_DataBase( Stock* stock )
+{
+    printf("");
+    //Stock_Add( stock, &p, cant);
 }
 
 //----------------------------------------------------------------------
@@ -298,7 +347,6 @@ int main()
    strcpy( p.nombre, "Choco Roles" );
 
    Stock_add( Inventario, &p, 10 );
-
 
    Stock_delete( stock );
    // destruye al inventario
